a888d829e75a325654e9ff6337f5627aaa3bf27b
LejoLevan <63107026+LejoLevan@users.noreply.github.com>
2021-07-18 21:47:22 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -4,7 +4,7 @@ class Settings: # pylint: disable=too-few-public-methods
     """A class to store all settings for game"""
     def __init__(self):
         """Initialize the game's settings"""
-        self.screen_width = 2560
-        self.screen_height = 1440
+        self.screen_width = 1920
+        self.screen_height = 1080
         self.bg_color = (0, 0, 0)
         self.txtSpeed = .055
